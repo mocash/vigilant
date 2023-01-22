def97d841cdac5752d521e92f6e1529983497473
mocash <m.siraji1@gmail.com>
2023-01-22 03:30:15 -0500

    first commit2

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+// const {
+//   getGoals,
+//   setGoal,
+//   updateGoal,
+//   deleteGoal,
+// } = require('../controllers/goalController')
+
+// const { protect } = require('../middleware/authMiddleware')
+
+// router.route('/').get(protect, getGoals).post(protect, setGoal)
+// router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
+router.get('/',(req,res)=>{
+    res.send('hello from goals')
+})
+module.exports = router
